a53185ae46089522147ebe029f0a5076bae53f6c
Ahmad Abdan <ahmad.abdan@technexus.ae>
2025-08-10 18:00:21 +0400

    Refactor DynamicListProfile for clarity and cleanup
    Streamlined the constructor of the `DynamicListProfile` class by including mapping configurations directly within it. Removed unused `using` directives to enhance code readability and reduce clutter.

--- Nexus.LAS.Application/MappingProfiles/DynamicListProfile.cs
+++ Nexus.LAS.Application/MappingProfiles/DynamicListProfile.cs
@@ -2,24 +2,17 @@
 using Nexus.LAS.Application.DTOs.Base;
 using Nexus.LAS.Application.DTOs.DynamicListDtos;
 using Nexus.LAS.Application.UseCases.DynamicListUseCases.Commands;
-using Nexus.LAS.Application.UseCases.Queries.GetDynamicListDto;
 using Nexus.LAS.Domain.Entities.Lookup;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Nexus.LAS.Application.MappingProfiles
+namespace Nexus.LAS.Application.MappingProfiles;
+
+public class DynamicListProfile:Profile
 {
-    public class DynamicListProfile:Profile
+    public DynamicListProfile() 
     {
-        public DynamicListProfile() 
-        {
-            CreateMap<CreateDynamicListCommand, DynamicList>();
-            CreateMap<UpdateDynamicListCommand, DynamicList>();
-            CreateMap<DynamicList, DynamicListDTO>();
-            CreateMap<PagingResult<DynamicList>, PagingResult<DynamicListDTO>>();
-        }
+        CreateMap<CreateDynamicListCommand, DynamicList>();
+        CreateMap<UpdateDynamicListCommand, DynamicList>();
+        CreateMap<DynamicList, DynamicListDTO>();
+        CreateMap<PagingResult<DynamicList>, PagingResult<DynamicListDTO>>();
     }
 }
