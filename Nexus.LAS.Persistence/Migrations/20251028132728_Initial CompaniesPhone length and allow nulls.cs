2362f1a5c954b5a713d669eadad1081250a463dd
Ahmad Abdan <ahmad.abdan@technexus.ae>
2025-10-28 17:28:47 +0400

    Update database schema for Companies and related entities
    Modifications include removing and altering several properties in the `Companies` entity and related tables. Key changes involve dropping columns related to expiration reminders and dates, adjusting properties to allow null values, and updating data types. A new migration class has been added to implement these changes and reflect the updated model snapshot.

---
+++ Nexus.LAS.Persistence/Migrations/20251028132728_Initial CompaniesPhone length and allow nulls.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexus.LAS.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCompaniesPhonelengthandallownulls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CciExpiryActiveReminder",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "CciExpiryDate",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "CciIssueDate",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "CciNumber",
+                table: "Companies");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ShareHolderNumbersOfShares",
+                table: "CompaniesShareHolders",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "CompaniesPhones",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(14)",
+                oldMaxLength: 14);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Designation",
+                table: "CompaniesPersonInCharges",
+                type: "int",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CommencementDate",
+                table: "CompaniesContracts",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClassOfShares",
+                table: "CompaniesCapitals",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ShareHolderNumbersOfShares",
+                table: "CompaniesShareHolders",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "CompaniesPhones",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Designation",
+                table: "CompaniesPersonInCharges",
+                type: "int",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CommencementDate",
+                table: "CompaniesContracts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClassOfShares",
+                table: "CompaniesCapitals",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CciExpiryActiveReminder",
+                table: "Companies",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CciExpiryDate",
+                table: "Companies",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CciIssueDate",
+                table: "Companies",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CciNumber",
+                table: "Companies",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+    }
+}
